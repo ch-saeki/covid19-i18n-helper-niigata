1bcb00455a37236327a2c2faedab41df23df510f
ch-saeki <sks.goo.c@gmail.com>
2020-03-29 13:01:45 +0900

    refactor:autopepで整形。

--- i18n_helper.py
+++ i18n_helper.py
@@ -3,7 +3,8 @@ from googleapiclient.discovery import build
 from google.auth.transport.requests import Request
 from google_auth_oauthlib.flow import InstalledAppFlow
 
-import os, sys
+import os
+import sys
 import math
 import json
 import glob
@@ -20,20 +21,23 @@ GSCOL_VI = 6
 GSCOL_NE = 7
 GSCOL_RU = 8
 
+
 def i18n_read_json(json_path):
     '''　i18njsonを順序保持したまま読込。 
     '''
     with open(json_path, 'r', encoding='utf-8') as lj:
         return json.loads(lj.read(), object_pairs_hook=OrderedDict)
 
+
 def i18n_get_gs(google_spread_sheet_url):
     ''' i18n google spread sheetをdfとして取得。 
-    '''    
+    '''
     return pd.read_csv(google_spread_sheet_url)
+
 
 def i18n_get_gs_org_words(gs):
     ''' 翻訳spread sheetからオリジナル文言リストを取得。
-        (ひとまず新潟県版翻訳を優先してkeyにする)
+        (ひとまず、新潟県版翻訳があれば優先してkeyにする)
     '''
     gscolumns = gs.columns.values.tolist()
     org_words = gs[gscolumns[GSCOL_ORG]].values.tolist()
@@ -43,6 +47,7 @@ def i18n_get_gs_org_words(gs):
             org_words[i] = niigata_words[i]
     org_words.pop(0)
     return org_words
+
 
 def i18n_get_gs_words(json_fname, gs):
     ''' 翻訳spread sheetから対象言語の文言リストを取得。
@@ -63,6 +68,7 @@ def i18n_get_gs_words(json_fname, gs):
     gs_words.pop(0)
     return gs_words
 
+
 def i18n_translated_check(source_json, target_json):
     """ source_jsonにある文章が翻訳されているかチェックし、
         target_jsonに存在しなければログ出力。 
@@ -72,18 +78,20 @@ def i18n_translated_check(source_json, target_json):
     target_lang = i18n_read_json(target_json)
     for src_word in src_lang.keys():
         if src_word not in target_lang.keys():
-            print(f"{os.path.basename(target_json)} \"{src_word}\" Translation not found.")
+            print(
+                f"{os.path.basename(target_json)} \"{src_word}\" Translation not found.")
             not_translation_list.append(src_word)
     return not_translation_list
 
+
 def i18n_find_word_from_json(gs, json_path):
     """　spread sheetにあってjsonにない該当言語の文章を探す。
-    """        
+    """
     print(f'Find google spread sheet words from {json_path}... ')
     not_found_lsit = []
 
     json_words = i18n_read_json(json_path)
-    json_fname = os.path.basename(json_path)    
+    json_fname = os.path.basename(json_path)
     gs_words = i18n_get_gs_words(json_fname, gs)
     for gw in gs_words:
         if gw != gw:
@@ -94,22 +102,24 @@ def i18n_find_word_from_json(gs, json_path):
             not_found_lsit.append(gw)
     return not_found_lsit
 
+
 def i18n_find_word_from_gs(json_path, gs):
     """ jsonにあってspread sheetにない文章を探す。    
         新潟県版翻訳が存在しないケースもあるので、ja.jsonの場合は2列から検索。 
-    """    
-    print(f'Find {json_path} words from spread sheet... ')  
-    not_found_lsit = []  
-    
+    """
+    print(f'Find {json_path} words from spread sheet... ')
+    not_found_lsit = []
+
     json_words = i18n_read_json(json_path).items()
-    json_fname = os.path.basename(json_path)    
-    gs_words = i18n_get_gs_words(json_fname, gs)    
+    json_fname = os.path.basename(json_path)
+    gs_words = i18n_get_gs_words(json_fname, gs)
     for jw in json_words:
         if jw[1] not in gs_words:
             """ TODO spread sheet側文章には「,」がない？"""
             print(f"{json_fname} \"{jw[1]}\" not found in spread sheet.")
             not_found_lsit.append(jw[1])
     return not_found_lsit
+
 
 def i18n_json_words_check(resource_files):
     """ localesディレクトリ配下のi18njsonについて、
@@ -119,8 +129,10 @@ def i18n_json_words_check(resource_files):
     for resource in resource_files:
         if 'ja.json' in resource:
             continue
-        not_translation_lists.append(i18n_translated_check('./locales/ja.json', resource))
+        not_translation_lists.append(
+            i18n_translated_check('./locales/ja.json', resource))
     return not_translation_lists
+
 
 def i18n_diff_json_and_gs(google_spread_sheet_url, json_files):
     """ ローカルのjson,google_spread_sheetのリソース差分(文言有無)を表示する。 
@@ -130,25 +142,27 @@ def i18n_diff_json_and_gs(google_spread_sheet_url, json_files):
         i18n_find_word_from_json(gs, resource)
         i18n_find_word_from_gs(resource, gs)
 
+
 def i18n_unused_check(vue_root, json_file):
     """ 指定ディレクトリ配下のvueソースから、
         使用されていない文言をリストアップ。
     """
     abspath = os.path.abspath(vue_root)
     vues = glob.glob(abspath+'/**/*.vue', recursive=True)
-    
+
     target_words = i18n_read_json(json_file)
     unused_list = list(target_words.keys())
     for vue in vues:
         ld = open(vue, encoding='utf-8')
         lines = ld.readlines()
-        ld.close()        
-        for word in unused_list:            
+        ld.close()
+        for word in unused_list:
             for line in lines:
                 if line.find(word) >= 0:
                     unused_list.pop(unused_list.index(word))
-                    break        
+                    break
     print(unused_list)
+
 
 def i18n_create_json(dst_json_path, base_words, gs):
     ''' google spread sheetから対象言語のi18n jsonを出力。
@@ -157,7 +171,7 @@ def i18n_create_json(dst_json_path, base_words, gs):
     od = OrderedDict()
     for i, word in enumerate(base_words):
         if word != word:
-            continue        
+            continue
         if target_words[i] != target_words[i]:
             if 'ja.json' in dst_json_path:
                 od[word] = word
@@ -173,9 +187,10 @@ def i18n_create_json(dst_json_path, base_words, gs):
                 wl = {target_words[i-1]: target_words[i]}
                 od[word] = wl
 
-    ''' output json''' 
+    ''' output json'''
     with open(dst_json_path, 'w', encoding='utf-8') as f:
         json.dump(od, f, indent=2, ensure_ascii=False)
+
 
 def i18n_create_json_from_gs(google_spread_sheet_url, dst_dir):
     ''' google spread sheetから各言語向けi18n jsonを出力。 
@@ -187,19 +202,20 @@ def i18n_create_json_from_gs(google_spread_sheet_url, dst_dir):
     for dst_json in dst_jsons:
         dst_json = os.path.join(dst_dir, dst_json)
         i18n_create_json(dst_json, base_words, gs)
-    
+
     # i18n_unused_check('/Users/saeki/dev/covid19', 'locales/ja.json')
     return
+
 
 def main():
     sheet_url = "https://docs.google.com/spreadsheets/d/1hcU7fT2peAlKNH5Dcp9xufuXH4vf5FGsRKxcF9WC9zc/export?format=csv&gid=17066585"
     # upload_test_url = "https://docs.google.com/spreadsheets/d/14283nuNCBeSNe4M6HSdar_9oVug2p15mkCNLwRrb4JQ/edit#gid=0"
     # upload_test_sheet_id = "14283nuNCBeSNe4M6HSdar_9oVug2p15mkCNLwRrb4JQ"
     resource_files = glob.glob('./out_locales/ja.json')
-    
+
     # ローカルリソース一貫性チェック
     # i18n_json_words_check(resource_files)
-    
+
     # ローカルのvueソース - json i18nリソースを比較、利用されていないリソース有無のチェック
     # i18n_unused_check('/Users/saeki/dev/covid19', 'locales/ja.json')
 
@@ -210,5 +226,6 @@ def main():
     dst_dir = './out_locales'
     i18n_create_json_from_gs(sheet_url, dst_dir)
 
-if __name__ =='__main__':
+
+if __name__ == '__main__':
     main()
